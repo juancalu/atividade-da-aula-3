968b01716aefc1ab398beba8ac0dd9ed6c1f97ec
juancalu <168238278+juancalu@users.noreply.github.com>
2024-05-03 19:14:04 -0300

    Add files via upload

---
+++ Desafio.py
@@ -0,0 +1,24 @@
+# **DESAFIO** 
+
+# **CALCULADORA SÓ COM *VARIÁVEIS E PRINT***
+
+# **4 OPERAÇÕES(+, -, *,/ )
+
+# Não utilizar  condicionais 
+
+# Não utilizar  funções da linguagem
+
+# Usar apenas variáveis, input() e print()
+n1 = float(input('Digite um numero: '))
+n2 = float (input('Digite outro numero: '))
+soma = n1+n2
+subtracao = n1-n2
+divisao = n1/n2
+multiplicacao = n1*n2
+print('Soma:', soma)
+print('Subtração:', subtracao)
+print('Multiplicação:', multiplicacao)
+print('Divisão:', divisao)
+
+
+
